135b6bded4dcd7a75e27b4d01337baacdca8062a
Shivam verma <shivamverma@Shivams-MacBook-Air-2.local>
2024-01-29 10:45:49 +0530

    val, var type casting, operators c1

--- Hello.scala
+++ Hello.scala
@@ -2,5 +2,21 @@ object Hello {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
     print(3)
+
+    // immutable variable
+    val message:String ="Hello Wrold"
+    // message="new message"    --> Reassignment to val message
+
+    // mutable variable
+    var m2:String="Hello world"
+    m2="hi"
+    print(m2)
+
+
+    //type casting
+    var a:Long=98765
+    var b:Float=a
+
+    //var c:Long=b  -->show error
   }
 }
